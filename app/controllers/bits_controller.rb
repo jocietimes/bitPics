6d75002a9268c225a140d4d59069dc245b176850
jocietimes <jocelyn.mellein@outlook.com>
2020-11-07 16:45:13 +0000

    create method

--- app/controllers/bits_controller.rb
+++ app/controllers/bits_controller.rb
@@ -5,4 +5,15 @@ class BitsController < ApplicationController
     def new
         @bit = Bit.new
     end
+
+    def create
+        @bit = Bit.create(bit_params)
+        redirect_to root_path
+    end
+
+    private
+
+    def bit_params
+        params.require(:bit).permit(:message)
+    end
 end
