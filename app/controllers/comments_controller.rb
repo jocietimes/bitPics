eab9f2beee324f715f2cf9f9c8b3db9b5186b159
jocietimes <jocelyn.mellein@outlook.com>
2020-11-10 03:50:46 +0000

    style

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,14 +3,10 @@ class CommentsController < ApplicationController
 
     def create
         @bit = Bit.find_by_id(params[:bit_id])
-        return render_not_found if @gram.blank?
+        return render_not_found if @bit.blank?
 
         @bit.comments.create(comment_params.merge(user: current_user))
-        if @comment.valid?
-            redirect_to root_path
-        else
-            render :new, status: :unprocessable_entity
-        end
+        redirect_to root_path
     end
 
     private
